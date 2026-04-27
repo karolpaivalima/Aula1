d7bb007c88aab32aa5f1ffb458e98b985561dbee
Karoline Carvalho Paiva Terceiro Lima <karolinepaivaterceiro@gmail.com>
2026-04-27 15:19:34 -0300

    aaaa

--- Main.js
+++ Main.js
@@ -1,1 +1,13 @@
-alert("Esse é um alerta!")
+let titulo = document.querySelector("h1")
+let input = document.querySelector("#titulo")
+
+function alterarTitulo(){
+    if (input.value !== ""){
+        titulo.innerText = input.value
+    } else {
+        alert("Preencha a porcaria da input, seu animal!")
+    }
+}
+
+let botao = document.querySelector("button")
+botao.addEventListener("click", alterarTitulo)
